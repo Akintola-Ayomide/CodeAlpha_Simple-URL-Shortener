1dd100db3bb6e28e11d6d0c17a4b13c1001881ff
Ayothega <Ayothega@gmail.com>
2026-04-27 09:47:33 +0100

    setup frontend folder

---
+++ client/src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="flex min-h-screen items-center justify-center bg-gray-100">
+      <h1 className="text-3xl font-bold text-blue-600">
+        React + TypeScript + Tailwind CSS Setup
+      </h1>
+    </div>
+  )
+}
+
+export default App
